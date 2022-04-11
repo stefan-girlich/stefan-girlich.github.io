c04565bcfe8ac7b71e790fe1239fd679f8b8ea14
Stefan Girlich <stefan.girlich@gmail.com>
2022-04-11 15:08:40 +0200

    Add expand/collapse button for career items

--- src/components/CareerHistory/CareerHistoryListItem.tsx
+++ src/components/CareerHistory/CareerHistoryListItem.tsx
@@ -1,15 +1,24 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 import { H4 } from '../Header/Heading'
 import Paragraph from '../Paragraph/Paragraph'
 import CareerHistoryTimelineSegment from './CareerHistoryTimelineSegment'
+
+import texts from '../../config/texts.json'
 
 const Root = styled.li`
   display: flex;
   flex-direction: row;
 `
 
-const CareerListItemContent = styled.div`
+const Content = styled.div`
   flex: 1;
+`
+
+const Details = styled.div`
+  max-height: 500px; // TODO a hack to avoid measuring content height - should be replaced when content is taller
+  transition: max-height 0.5s ease-out;
+  overflow: hidden;
 `
 
 const TitleRow = styled(H4)`
@@ -20,12 +29,28 @@ const TitleRow = styled(H4)`
   margin-bottom: ${({ theme }) => theme.spacing(1)};
 `
 
+const TitleRowLefthandItems = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
 const Role = styled.span`
   /* display: inline-block; */
 `
 const Organization = styled.span`
   color: ${({ theme }) => theme.palette.secondary.main};
   font-weight: 300;
+`
+
+const ExpandCollapseButton = styled.button`
+  color: ${({ theme }) => theme.palette.primary.main};
+  border: 0;
+  background: transparent;
+  font-weight: 300;
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+  margin-left: ${({ theme }) => theme.spacing(1)};
 `
 
 const Timespan = styled.div`
@@ -66,32 +91,40 @@ interface Props {
 }
 
 const CareerHistoryListItem = ({ data, index }: Props) => {
+  const [isExpanded, setExpanded] = useState(false)
+  const toggle = () => setExpanded(!isExpanded)
+
   return (
     <Root>
       <CareerHistoryTimelineSegment isMostRecent={index === 0} />
 
-      <CareerListItemContent>
+      <Content>
         <TitleRow>
-          <div>
+          <TitleRowLefthandItems>
             <Role>{data.role}</Role>
             <Organization>&nbsp;&nbsp;&mdash;&nbsp;&nbsp;{data.organization}</Organization>
-          </div>
+            <ExpandCollapseButton onClick={toggle}>
+              {isExpanded ? texts.show_less : texts.show_more}
+            </ExpandCollapseButton>
+          </TitleRowLefthandItems>
 
           <Timespan>
             {data.start_year} - {data.end_year}
           </Timespan>
         </TitleRow>
 
-        <Paragraph>{data.text}</Paragraph>
+        <Details style={isExpanded ? undefined : { maxHeight: 0 }}>
+          <Paragraph>{data.text}</Paragraph>
 
-        <ActivityList>
-          {data.activities.map((activity, activityIndex) => (
-            <ActivityListItem as="li" key={`${index}_${activityIndex}`}>
-              {activity}
-            </ActivityListItem>
-          ))}
-        </ActivityList>
-      </CareerListItemContent>
+          <ActivityList>
+            {data.activities.map((activity, activityIndex) => (
+              <ActivityListItem as="li" key={`${index}_${activityIndex}`}>
+                {activity}
+              </ActivityListItem>
+            ))}
+          </ActivityList>
+        </Details>
+      </Content>
     </Root>
   )
 }
